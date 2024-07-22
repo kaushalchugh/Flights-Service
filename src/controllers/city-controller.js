d0dc5684a5f627c38cbae968bc1b41d10d9632aa
kaushalchugh <kaushalchugh22@gmail.com>
2024-07-23 00:49:46 +0530

    Implemented the Delete City Api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -62,8 +62,30 @@ async function getCity(req, res) {
                 .json(ErrorResponse);
     }
 }
+
+/**
+ * DELETE : /cities/:id
+ * req-body {}
+ */
+async function destroyCity(req, res) {
+    try{
+        const city = await CityService.destroyCity(req.params.id);
+        SuccessResponse.data = city;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
+
+
 module.exports = {
     createCity,
     getCities,
-    getCity
+    getCity,
+    destroyCity
 }
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -42,8 +42,21 @@ async function getCity(id) {
     }
 }
 
+async function destroyCity(id) {
+    try {
+        const response = await cityRepository.destroy(id);
+        return response;
+    } catch(error) {
+        if(error.statusCode == StatusCodes.NOT_FOUND) {
+            throw new AppError('The City you requested to delete is not present', error.statusCode);
+        }
+        throw new AppError('Cannot fetch data of all the cities', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
 module.exports = {
     createCity,
     getCities,
     getCity,
+    destroyCity
 }
