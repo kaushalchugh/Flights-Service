cc2d9d311580b70ddbbb352efbbc9a919fc8c795
kaushalchugh <kaushalchugh22@gmail.com>
2024-07-31 23:34:09 +0530

    Added seats model in our db associated to airplane

---
+++ src/utils/common/enums.js
@@ -0,0 +1,10 @@
+const SEAT_TYPE = {
+    BUSINESS: 'Business',
+    ECONOMY: 'Economy',
+    PREMIUM_ECONOMY: 'Premium-Economy',
+    FIRST_CLASS: 'First-Class'
+}
+
+module.exports = {
+    SEAT_TYPE
+}
